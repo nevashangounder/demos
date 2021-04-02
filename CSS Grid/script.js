8ab88169248a8708703645e14c6f4a591fba1553
nevashangounder <64774744+nevashangounder@users.noreply.github.com>
2021-04-02 06:02:09 +0200

    Add files via upload

---
+++ CSS Grid/script.js
@@ -0,0 +1,13 @@
+let number = 16;
+
+
+
+for(let i = 1; i < 20; i++){
+    let container = document.getElementById("container")
+    let newDiv = document.createElement("div");
+    container.appendChild(newDiv).classList.add("cell-2");
+    
+}
+
+
+
